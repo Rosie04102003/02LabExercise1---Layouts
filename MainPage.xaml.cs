027ae881e7079c16900d344c33cde89939687b19
Rosie04102003 <69665835+Rosie04102003@users.noreply.github.com>
2023-09-20 13:57:09 +0800

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿namespace MauiApp2;
+
+public partial class MainPage : ContentPage
+{
+	String x = "Reality";
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void OnImageButton1Clicked(object sender, EventArgs e)
+	{
+		Expectation1.Source = "re1.jpg";
+		lbl1.Text = x;
+	}
+
+	private void OnImageButton2Clicked(object sender, EventArgs e)
+	{
+		Expectation2.Source = "re2.jpg";
+		lbl2.Text = x;
+	}
+
+
+}
